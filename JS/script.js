b5404123dfdb6401e69108df437400279ea5e0ff
Christine Shine <x20211651@student.ncirl.ie>
2021-12-04 16:47:42 +0000

    Structure

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+//<button onclick="create()">Terms & Conditions</button>
+//			<button onclick="Privacy()">Privacy</button>
+//			<button onclick="Covid()">Covid Update</button>
+
+function create(){
+	alert(" You should check the Terms of Use regularly to ensure that you understand and are familiar with any changes. You will be deemed to have accepted any changes to the Terms of Use after you have been notified of the changes on our Website home page and you continue to access or use the Website.")
+}
+function Privacy(){
+		alert(" Under GDPR, the main grounds that we rely upon in order to process your Information are the following:Necessary for compliance with a legal obligation – we are subject to certain legal requirements which may require us to process your Information. We may also be obliged by law to disclose your Information to a regulatory body or law enforcement agency;ith any changes. You will be deemed to have accepted any changes to the Terms of Use after you have been notified of the changes on our Website home page and you continue to access or use the Website.")
+
+}
+function Covid(){
+		alert("It is important that businesses should check photo ID when checking Proof of Immunity. There are some limited circumstances when this may not be necessary e.g. where a person is well-known to the business. Unaccompanied minors are required to present Proof of Immunity. Outdoor Hospitality can continue to operate for customers who are unable to provide evidence of COVID-19 vaccination or immunity following recovery. A maximum of 15 people including no more than 10 adults may be seated at a table.")
+
+}
