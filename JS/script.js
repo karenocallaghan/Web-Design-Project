225e8c492896181c6a9386b4a6b4363691c0c6c6
Christine Shine <x20211651@student.ncirl.ie>
2021-12-12 12:26:28 +0000

    5th Christine

--- JS/script.js
+++ JS/script.js
@@ -25,6 +25,38 @@ function Covid(){
 		alert("It is important that businesses should check photo ID when checking Proof of Immunity. There are some limited circumstances when this may not be necessary e.g. where a person is well-known to the business. Unaccompanied minors are required to present Proof of Immunity. Outdoor Hospitality can continue to operate for customers who are unable to provide evidence of COVID-19 vaccination or immunity following recovery. A maximum of 15 people including no more than 10 adults may be seated at a table.")
 
 }
-$(function() {  
-            $( "#datepicker-1" ).datepicker();  
-         }  
+
+function submit(){
+	//event.preventDefault();
+	var x = document.getElementById("name").value;
+	//var at = document.getElementById("email").value;
+	//var y = document.getElementById("datetimepicker2").value;
+	//var z= document.getElementById("datetimepicker1").value;
+	
+	alert(x)
+	/*var a=parseInt(document.getElementById("person_number").value);
+	var b=parseInt(document.getElementById("res_facilities").value);
+	
+	if (a <7 && b=1){
+		alert("we will confirm your booking on" +y+ " via "+at)
+	}
+	else if (a <7 && b=2){
+		alert("Unfortunately, unvaccinated parties must dine outdoors, we don't have outdoor dining.  apologies ")
+	}
+	else if(a >=7){
+		alert(" Current Covid restrictions only allows a table of 6 or less, You entered " +a+ "please retry with a smaller party  ")
+	
+	}
+	else (){
+		alert("Covid Guidelines currently allows only vaccinated parties to sit indoors, with a limit of 6 on the table ")
+	
+	}*/
+}
+/*
+event.preventDefault();
+	var x = document.getElementById("name").value;
+	var at = document.getElementById("email").value;
+	var y = document.getElementById("date").value;
+	var z= document.getElementById("time").value;
+	var a=parseInt(document.getElementById("person_number").value);
+	*/
